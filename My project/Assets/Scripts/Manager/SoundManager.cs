d4b7ce80ae3b5c7b07d68326695e89d6a759ac3e
admin <answn4450@gmail.com>
2023-08-24 21:51:05 +0900

    import resource
    import "FREE Casual Game SFX Pack" 를 "CasualGameSounds" 폴더에

---
+++ My project/Assets/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; } = null;
+
+    public AudioSource audioSource;
+    public Dictionary<string, AudioClip> soundDictionary;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        // ** 초기화
+        audioSource = GetComponent<AudioSource>();
+        soundDictionary = new Dictionary<string, AudioClip>();
+    }
+
+    public void PlaySound(string _key)
+    {
+        if (!soundDictionary.ContainsKey(_key))
+        {
+            Debug.Log("없음..");
+            return;
+        }
+
+        var clip = soundDictionary[_key];
+        audioSource.PlayOneShot(clip);
+    }
+
+
+    public void PlayBGM(string _key)
+    {
+        if (!soundDictionary.ContainsKey(_key))
+        {
+            Debug.Log("BGM 없음..");
+            return;
+        }
+
+        audioSource.clip = soundDictionary[_key];
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+
+    public void StopBGM()
+    {
+        audioSource.Stop();
+    }
+
+    public void AddSoundClip(AudioClip _clip)
+    {
+        if (soundDictionary.ContainsKey(_clip.name))
+        {
+            Debug.Log("이미 있음..");
+            return;
+        }
+
+        soundDictionary[_clip.name] = _clip;
+    }
+
+
+    void Start()
+    {
+        // ** 사운드 불러오기.
+
+
+    }
+}
